83e7514f8d969d15f553aa4f8a0a3041a72cf5ec
Mike <art_is_me@mail.ru>
2026-02-04 08:51:11 +0300

    init

---
+++ labs/0/m.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <chrono>
+
+int main()
+{
+  std::vector<size_t> v = { 1, 2, 3, 4, 5, 6, 7, 8};
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::sort(v.begin(), v.end());
+    do
+    {
+        copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+        std::cout << std::endl;
+    } while (std::next_permutation(v.begin(), v.end()));
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Duration: " << duration.count() / 1000.f << " s\n";
+}
+//g++ m.cpp -o m.exe
