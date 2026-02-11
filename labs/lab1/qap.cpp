f7a41465a369f26b62e5b7dceee2e1bda049d9e1
Mike <art_is_me@mail.ru>
2026-02-11 15:41:00 +0300

    lab1

--- labs/lab1/qap.cpp
+++ labs/lab1/qap.cpp
@@ -9,8 +9,6 @@
 #include "rmff.cpp"
 #include "matrixCreator.cpp"
 
-// функция подсчета времени t * 10 * 11 * 12 для матрицы 12
-
 using namespace std;
 
 using matrix = vector<vector<int>>;
@@ -26,18 +24,30 @@ unsigned int calculateR(matrix& Q, matrix& C, vector<int>& idxOrder, int n)
     return R;
 }
 
+void calculateTime(size_t n)
+{
+    double dt10 = 2.866;
+    for (size_t i = 11; i <= n; ++i)
+    {
+        dt10 *= i;
+    }
+    cout << "Time: " << (dt10 / (3600 * 24 * 365)) << " years" << endl;
+}
+
 int main(int argc, char const *argv[]) 
 {
+    if (argc == 2) 
+    {
+        const int n = atoi(argv[1]);
+        calculateTime(n);
+        return 0;
+    }
     if (argc != 3) throw "wrong number of args";
+
     matrix Q = readMatrixFromFile(argv[1]);
     matrix C = readMatrixFromFile(argv[2]);
 
     const int n = C.size();
-    if (n > 15) 
-    {
-        //запустить f расчета времени
-        return 0;
-    }
 
     vector<int> idxOrder(n);
     for (int i = 0; i < n; ++i) idxOrder[i] = i;
--- labs/lab1/test.bat
+++ labs/lab1/test.bat
@@ -1,11 +1,11 @@
 @echo off
-setlocal enabledelayedexpansion
+@REM setlocal enabledelayedexpansion
 set PROG=qap.exe
-set "RESULT="
 
 echo ------------------------------------------
 echo calculate matrix 4x4
 set "EXPECTED=5050"
+set "RESULT="
 for /f "delims=" %%i in ('%PROG% .\data\qm4x4.txt .\data\cm4x4.txt 2^>nul') do (
     echo %%i
     if not defined RESULT set "RESULT=%%i"
@@ -15,6 +15,7 @@ if "%RESULT%"=="%EXPECTED%" (echo [OK] valid) else (goto err)
 echo ------------------------------------------ 
 echo swap matrix
 set "EXPECTED=5050"
+set "RESULT="
 for /f "delims=" %%i in ('%PROG% .\data\cm4x4.txt .\data\qm4x4.txt 2^>nul') do (
     echo %%i
     if not defined RESULT set "RESULT=%%i"
@@ -25,11 +26,25 @@ echo ------------------------------------------
 echo diagonal != 0
 echo ------------------------------------------
 echo calculate matrix 10x10 
+set "EXPECTED=42386"
+set "RESULT="
+for /f "delims=" %%i in ('%PROG% .\data\qm10x10.txt .\data\cm10x10.txt 2^>nul') do (
+    echo %%i
+    if not defined RESULT set "RESULT=%%i"
+)
+set "RESULT=%RESULT: =%"
+if "%RESULT%"=="%EXPECTED%" (echo [OK] valid) else (goto err)
 echo ------------------------------------------
 echo calculate matrix 1x1 
 echo ------------------------------------------
 echo calculate matrix 2x2 
-
+echo ------------------------------------------
+echo calculate matrix 20x20
+%PROG% 20
+echo ------------------------------------------
+echo calculate matrix 50x50
+%PROG% 50
+echo Universe age: 1.38e+10
 exit /b 0
 
 :err
